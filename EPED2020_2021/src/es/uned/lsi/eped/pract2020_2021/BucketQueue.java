92954b06a1e0007f8b602d4fe15f73ecd55261db
MartinMZR <MartinMZR@LEVIATAN>
2021-04-09 08:43:44 +0200

    Primera aproximación en SamePriority. Falta averiguar como resuelvo el iterador Falta averiguar como enlazo los nodos en la cola

--- EPED2020_2021/src/es/uned/lsi/eped/pract2020_2021/BucketQueue.java
+++ EPED2020_2021/src/es/uned/lsi/eped/pract2020_2021/BucketQueue.java
@@ -2,23 +2,31 @@ package es.uned.lsi.eped.pract2020_2021;
 
 import es.uned.lsi.eped.DataStructures.Collection;
 import es.uned.lsi.eped.DataStructures.IteratorIF;
+import es.uned.lsi.eped.DataStructures.Sequence;
 
 /*Representa una cola con prioridad implementada mediante una secuencia de SamePriorityQueue*/
 public class BucketQueue<E> extends Collection<E> implements PriorityQueueIF<E> {
  
   //LA DEFINICIÓN DE LOS ATRIBUTOS DE LA CLASE ES TAREA DE CADA ESTUDIANTE
 
+	private E value;
+	
   /* Clase privada que implementa un iterador para la *
    * cola con prioridad basada en secuencia.          */
   public class PriorityQueueIterator implements IteratorIF<E> {
 
     //LA DEFINICIÓN DE LOS ATRIBUTOS DE LA CLASE ES TAREA DE CADA ESTUDIANTE
-
+	  
+	  private E currentNode;
+	  
     /*Constructor por defecto*/
-    protected PriorityQueueIterator(){ … }
+    protected PriorityQueueIterator(){ ... }
 
     /*Devuelve el siguiente elemento de la iteración*/
-    public E getNext() { … }
+    public E getNext() { 
+
+    	E elem = this.currentNode.
+    }
     
     /*Comprueba si queda algún elemento por iterar*/
     public boolean hasNext() { … }
@@ -36,6 +44,7 @@ public class BucketQueue<E> extends Collection<E> implements PriorityQueueIF<E> 
   BucketQueue() { 
 	  
 	super();  
+	this.lastNode = null;
   }
 
   /* OPERACIONES PROPIAS DE LA INTERFAZ PRIORITYQUEUEIF */
@@ -60,21 +69,33 @@ public class BucketQueue<E> extends Collection<E> implements PriorityQueueIF<E> 
   /* OPERACIONES PROPIAS DE LA INTERFAZ SEQUENCEIF */
 
   /*Devuelve un iterador para la cola*/
-  public IteratorIF<E> iterator() { … }
+  public IteratorIF<E> iterator() { 
+
+	  return new PriorityQueueIterator();	  
+  }
  
   /* OPERACIONES PROPIAS DE LA INTERFAZ COLLECTIONIF */
 
   /*Devuelve el número de elementos de la cola*/
-  public int size() { … }
+  public int size() { 
+
+	  return size;			// Atributo dentro tipo protected dentro de la interfaz collection
+  }
 
   /*Decide si la cola está vacía*/
-  public boolean isEmpty() { … }
+  public boolean isEmpty() { 
+	  
+	  return size == 0;		// Compara el tamaño del atributo size, y devuelve el resultado
+  }
  
   /*Decide si la cola contiene el elemento dado por parámetro*/
-  public boolean contains(E e) { … }
+  public boolean contains(E e) {...}
  
   /*Elimina todos los elementos de la cola*/
-  public void clear() { … }
+  public void clear() { 
+
+	  size = 0;				// Cambia el tamaño de la colección a 0
+  }
  
 }
 
--- EPED2020_2021/src/es/uned/lsi/eped/pract2020_2021/SamePriorityQueue.java
+++ EPED2020_2021/src/es/uned/lsi/eped/pract2020_2021/SamePriorityQueue.java
@@ -1,57 +1,136 @@
 package es.uned.lsi.eped.pract2020_2021;
 
 import es.uned.lsi.eped.DataStructures.QueueIF;
+import es.uned.lsi.eped.DataStructures.IteratorIF;
 
 /*Representa una cola con un nivel de prioridad asignado determinado*/
 public class SamePriorityQueue<E> implements QueueIF<E>,Comparable<SamePriorityQueue<E>>{
  
   //LA DEFINICIÓN DE LOS ATRIBUTOS DE LA CLASE ES TAREA DE CADA ESTUDIANTE
+	
+	private E currentNode;					// Almacena el valor del nodo actual
+	private E lastNode;						// Almacena el último nodo
+	private E firstNode; 					// Almacena el primer nodo
+	private E nextNode;  					// Almacena el nodo siguiente al actual
+	private int priorityLevel;				// Almacena el nivel de prioridad 
+	private int size;						// Almacena el tamaño de la cola
 
   /* OPERACIONES PROPIAS DE ESTA CLASE */
 
   /*constructor por defecto: crea cola vacía con la prioridad dada por parámetro.
    *@param p: nivel de prioridad asociado a la cola
   */
-  SamePriorityQueue(int p){ … }
+  SamePriorityQueue(int p){ 
+	  
+	  priorityLevel = p;
+	  firstNode = null;
+	  lastNode = null;
+	  size = 0;
+  }
 
   /* Devuelve la prioridad de la cola
    * @return prioridad de la cola
    */
-  public int getPriority(){ … }
+  public int getPriority(){ 
+	  
+	  return priorityLevel;
+  }
  
   /* OPERACIONES PROPIAS DE QUEUEIF */
 
   /*Devuelve el primer elemento de la cola
    * @Pre !isEmpty()
    */
-  public E getFirst() { … }
+  public E getFirst() { 
+	  
+	  try {
+		  
+		  return firstNode;
+	  
+	  } catch (NullPointerException e) {
+		  System.err.println("No se puede mostrar el primer elemento, la cola está vacía. ");
+		  return null;
+	  }
+  }
  
   /*Añade un elemento a la cola de acuerdo al orden de llegada*/
-  public void enqueue(E elem) { … }
+  public void enqueue(E elem) { 
+
+	  currentNode = elem;
+	  
+	  if(isEmpty()) {
+		  firstNode = currentNode;
+	  } else {
+		  nextNode = currentNode;
+	  }
+	  
+	  lastNode = currentNode;
+	  size++;
+  }
 
   /*Elimina un elemento a la cola de acuerdo al orden de llegada
    * @Pre !isEmpty()
   */
-  public void dequeue() { … }
+  public void dequeue() { 
+	  
+	  try {
+		  
+		  firstNode = nextNode;
+		  size--;
+		  
+		  if(size == 0) {
+			  lastNode = null;
+		  }
+		  
+	  } catch (NullPointerException e) {
+			System.err.println("Error, no se puede extraer el nodo, la cola está vacía.");
+	  }
+  }
 
   /* OPERACIONES PROPIAS DEL INTERFAZ SEQUENCEIF */
 
   /*Devuelve un iterador para la cola*/
-  public IteratorIF<E> iterator() { … }
+  public IteratorIF<E> iterator() {
+	  
+	  
+  }
  
   /* OPERACIONES PROPIAS DEL INTERFAZ COLLECTIONIF */
 
   /*Devuelve el número de elementos de la cola*/
-  public int size() { … }
+  public int size() { 
+	  
+	  return size;
+  }
 
   /*Decide si la cola está vacía*/
-  public boolean isEmpty() { … }
+  public boolean isEmpty() { 
+	  
+	  return size == 0;
+  }
  
   /*Decide si la cola contiene el elemento dado por parámetro*/
-  public boolean contains(E e) { … }
+  public boolean contains(E e) { 
+
+	  E node = firstNode;
+	  
+	  while(node != null) {
+		  if(node.equals(e)) {
+			  return true;
+		  }
+		  node = nextNode;
+	  }
+	  
+	  return false;
+  }
  
   /*Elimina todos los elementos de la cola*/
-  public void clear() { … }
+  public void clear() { 
+	  
+	  size = 0;
+	  firstNode = null;
+	  lastNode = null;
+  }
  
   /* OPERACIONES PROPIAS DEL INTERFAZ COMPARABLE */
  
@@ -61,7 +140,20 @@ public class SamePriorityQueue<E> implements QueueIF<E>,Comparable<SamePriorityQ
    *  - Valor 0 si ambas colas tienen la misma prioridad
    *  - Valor <0 si la cola tiene menor prioridad que la cola dada por parámetro
    */
-  public int compareTo(SamePriorityQueue<E> o) { … }
+  public int compareTo(SamePriorityQueue<E> o) { 
+	  
+	  int result;
+	  
+	  if(this.getPriority() > o.getPriority()) {		  
+		  result = 1;
+	  } else if(this.getPriority() == o.getPriority()) {		  
+		  result = 0; 
+	  } else { 		  
+		  result = -1; 
+	  }
+	  
+	  return result;	  
+  }
 
 }
 
